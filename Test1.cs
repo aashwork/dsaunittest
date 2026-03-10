e8540e6b3566cec5b9fd31a4d84f444a017b3746
aashwork <aashrayaivbhat@gmail.com>
2026-03-10 16:16:52 +0530

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSAUnitTestExample;
+
+namespace DSAUnitTestExample.Tests
+{
+    [TestClass]
+    public class ArrayUtilsTests
+    {
+        [TestMethod]
+        public void ArraySum_NormalInput_ReturnsSum()
+        {
+           
+            int[] input = { 1, 2, 3 };
+
+            
+            int result = ArrayUtils.ArraySum(input);
+
+           
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void ArraySum_EmptyArray_ReturnsZero()
+        {
+            
+            int[] input = { };
+
+            
+            int result = ArrayUtils.ArraySum(input);
+
+            
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void ArraySum_SingleElement_ReturnsElement()
+        {
+            
+            int[] input = { 5 };
+
+           
+            int result = ArrayUtils.ArraySum(input);
+
+            
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void ArraySum_NegativeNumbers_ReturnsCorrectSum()
+        {
+            
+            int[] input = { -1, -2, -3 };
+
+           
+            int result = ArrayUtils.ArraySum(input);
+
+            
+            Assert.AreEqual(-6, result);
+        }
+    }
+}
