28bc312fbd6713088999bfe80addd0138ebee077
aashwork <aashrayaivbhat@gmail.com>
2026-03-10 16:13:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DSAUnitTestExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ArrayUtils
+    {
+        public static int ArraySum(int[] arr)
+        {
+            int sum = 0;
+            foreach (int num in arr)
+                sum += num;
+            return sum;
+        }
+    }
+}
